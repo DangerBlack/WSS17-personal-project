5ed569e96c37e665470a5c0c2cd6968833530e09
DangerBlack <danger.recheng@hotmail.it>
2017-07-03 18:01:21 -0400

    Major refactoring, classification of examples, bugfix, removed bulbasaur dependencies

--- Project/whitelist.m
+++ Project/whitelist.m
@@ -32,7 +32,7 @@ extractAllSymbols[f_] :=
 	]
 
 (*Load all example from function in wholewhitelist in documentation*)
-loadAllExample[wholewhitelist_] := Map[extractFunctionFromRowBox, 
+loadAllExample[wholewhitelist_] := Flatten@Map[extractFunctionFromRowBox, 
  										Lookup[
  												WolframLanguageData[$wholewhitelist, "DocumentationExampleInputs"] 
  													/. _Missing -> {"BasicExamples" -> {}}, 
@@ -54,7 +54,8 @@ filterUnsafeExpression[l_,safeExprList_] := Select[safeExprList, SubsetQ[l, Last
 getCategoriesName[whitelist_]:=
 	Association @ KeyValueMap[buildKey[#1] -> #1 &, whitelist]
 
-loadCategories[whitelist_, examples_]:=
+
+(*loadCategories[whitelist_, examples_]:=
 	Association @ KeyValueMap[
 		Function[
 			{cat, symbols},
@@ -67,13 +68,36 @@ loadCategories[whitelist_, examples_]:=
 			]
 		],
 		whitelist
-	]
+	]*)
+weight[l_] := 
+ Association @@ Map[# -> Count[l, #]/Length[l] &, DeleteDuplicates@l]
+calculateWeight[categorySymbol_, symbols_] := 
+ N[Times @@ Map[Lookup[categorySymbol, #, 10^-6] &, symbols]]
+pickCategoryFromSymbol[symbols_] :=
+ 		With[{wl = Map[
+     			calculateWeight[#, symbols] &,
+     			KeyValueMap[#1 -> weight[#2] &, $whitelist][[All, 2]]
+     		]},
+  			First@Flatten@Position[wl, Max[wl]]
+  			]
+loadCategories[whitelist_, exampleSymbol_] :=
+ Association@With[{
+			    	catsid = Map[buildKey, Keys@whitelist]
+				    },
+				   With[{esLabeled = 
+				      Map[First[#] -> catsid[[pickCategoryFromSymbol[Last[#]]]] &, 
+				       exampleSymbol]},
+				      GroupBy[esLabeled, Last->First,Map[customHash]]
+			   		]
+				]
 
 
 saveExample[key_,example_]:=
 	Put[example,FileNameJoin[{$ApplicationExampleFolder, StringJoin[key,".m"]}]];
 getExample[key_]:=
-	Get[FileNameJoin[{$ApplicationExampleFolder, StringJoin[key,".m"]}]];
+	{
+		Get[FileNameJoin[{$ApplicationExampleFolder, StringJoin[key,".m"]}]]
+	};
 
 $DataBase := $DataBase = getSettings["dataset.m",
 	With[
