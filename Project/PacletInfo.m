204785140cf0c543d49856fb5c967b374849f1bc
DangerBlack <danger.recheng@hotmail.it>
2017-07-04 20:59:16 -0400

    bugfix and more description of the project detail

--- Project/PacletInfo.m
+++ Project/PacletInfo.m
@@ -2,8 +2,8 @@ Paclet[
 	Name -> "Project",
 	Version -> "0.0.1",
 	MathematicaVersion -> "11+",
-	Description -> "Project Library",
-	Creator -> "Riccardo Di Virgilio <riccardod@wolfram.com>",
+	Description -> "Project Library for WSS17, Complete the Wolfram Expression",
+	Creator -> "Daniele Baschieri <daniele.baschieri@gmail.com>",
 	Loading -> Automatic,
 	Extensions -> {
 		{"Kernel", Context -> {"Project`"}, Symbols -> {}},
